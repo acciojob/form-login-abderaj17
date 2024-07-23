ec3ee883f0dc9d34478d977e23b52f07260daf8a
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2024-07-23 21:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	let input1 = document.querySelector('[name="fname"]');
+	let input2 = document.querySelector('[name="lname"]');
+	let btn = document.querySelector('[type="submit"]');
 
+	btn.addEventListener('click', printName);
+
+	function printName(event) {
+                // Prevent the form from submitting and refreshing the page
+                event.preventDefault();
+		let output = alert(input1.value + " " + input2.value);
+	}
 }
